2a933787a100b1cfa84df6ed16d866043898906a
sannishamusideen <samsonanu@yahoo.com>
2024-05-23 15:01:53 +0100

    learning

--- asset/js/test.js
+++ asset/js/test.js
@@ -8,3 +8,39 @@ function substitute() {
     var myTitle = document.getElementById('title');
     myTitle.innerHTML = myValue;
 }
+
+// var myValue = "tabor"; //string
+// var myValue = 3.14;  //numbers
+
+//     alert(typeof myValue);
+
+
+
+
+// var myValue = 3.16;
+// var myOtherValue = 3.14;
+
+// var myResult = myValue + myOtherValue;
+
+// alert(myResult);
+
+
+// Boolean 
+// var myValue = true;
+
+// alert(typeof myValue);
+
+// Basic String Concatenation
+someValue = "Sanni" + " " + "Shamusideen";
+
+alert(someValue);
+
+wordValue = "My \"so called\" life."
+// wordValue = "I'm pretty sure this will work";
+alert(wordValue);
+
+newlineValue = "Sanni \rwas \nhere"
+// r- return character n- new line
+alert(newlineValue);
+
+
