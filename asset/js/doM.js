b2447fb2054234e61347bab5d104448777977c46
sannishamusideen <samsonanu@yahoo.com>
2024-05-28 12:38:06 +0100

    DOM Practice

---
+++ asset/js/doM.js
@@ -0,0 +1,48 @@
+window.onload = function() {
+// var clickMeButton = document.getElementById("clickMe");
+// clickMeButton.onclick = runTheExample;
+
+document.getElementById("clickMe").onclick = runTheExample;
+}
+
+// function runTheExample() {
+//     alert("running the example");
+// }
+
+// function runTheExample(){
+//     // var myElement = document.getElementById("second");
+
+//     // var myNodeName = myElement.nodeName;
+//     // alert(myNodeName);
+
+//     // if (myElement != null)
+//     //     {
+//     //         alert(myElement.innerHTML);
+//     //     }
+
+//     // document.getElementById("second").innerHTML = "See how I set the test here.";
+
+//     // var listOfParapraphs = document.getElementsByTagName("p");
+
+//     // alert(listOfParapraphs.length);
+
+//     // var secondParapraph = listOfParapraphs[1];
+
+//     // alert(secondParapraph.innerHTML);
+
+
+//     // myElement = document.getElementById("second");
+//     // alert(myElement.parentNode.nodeName);
+
+//     // myElement.childNodes[0];
+//     // myElement.firstChild;
+//     // myElement.lastChild;
+
+//     // myElement.nextSibling;
+//     // myElement.previousSibling;
+
+// // var anchor = document.getElementById("myAnchor");
+// // var anchorDestination = anchor.href;
+// // alert(anchorDestination);
+// // anchor.href = "http://www.bing.com";
+// }
